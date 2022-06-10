2e06b5eeb045917791272701227c6ad69721b25a
Rafael Martinelli <martinelli@puc-rio.br>
2022-06-10 17:32:40 -0300

    [#2] Bounds for Max and Min objective, calculated using a MIP Model.

--- src/AssignmentProblems.jl
+++ src/AssignmentProblems.jl
@@ -1,6 +1,7 @@
 module AssignmentProblems
 
 export AssignmentProblem, loadAssignmentProblem
+export na, nj
 
 const data_path = joinpath(pkgdir(AssignmentProblems), "data")
 
@@ -9,5 +10,14 @@ using ZipFile
 include("Data.jl")
 include("Util.jl")
 include("Loader.jl")
+include("Enums.jl")
+
+for inst in instances(Instance)
+    @eval export $(Symbol(inst))
+end
+
+for obj in instances(Objective)
+    @eval export $(Symbol(obj))
+end
 
 end
--- src/Loader.jl
+++ src/Loader.jl
@@ -1,4 +1,4 @@
-function loadAssignmentProblem(instance::Symbol)
+function loadAssignmentProblem(instance::Symbol, objective::Symbol = :Min)
     file_name = joinpath(data_path, string(instance) * ".zip")
     if !isfile(file_name)
         println("File $(string(instance)) not found!")
@@ -10,10 +10,10 @@ function loadAssignmentProblem(instance::Symbol)
     values = parse.(Int64, split(read(file.files[1], String)))
     close(file)
 
-    return load(values, name)
+    return load(values, name, objective)
 end
 
-function loadAssignmentProblem(file_name::String)
+function loadAssignmentProblem(file_name::String, objective::Symbol = :Min)
     if !isfile(file_name)
         println("File $file_name not found!")
         return nothing
@@ -22,15 +22,12 @@ function loadAssignmentProblem(file_name::String)
     name = splitext(basename(file_name))[1]
     values = parse.(Int64, split(read(file_name, String)))
 
-    return load(values, name)
+    return load(values, name, objective)
 end
 
-function load(values::Array{Int64}, name::String)
+function load(values::Array{Int64}, name::String, objective::Symbol)
     n_agents = values[1]
     n_jobs = values[2]
-
-    agents = collect(1:n_agents)
-    jobs = collect(1:n_jobs)
 
     counter = 3
 
@@ -48,5 +45,5 @@ function load(values::Array{Int64}, name::String)
 
     capacities = values[counter:counter + n_agents - 1]
 
-    return AssignmentProblem(name, agents, jobs, capacities, costs, consumptions, loadBounds(name)...)
+    return AssignmentProblem(name, capacities, costs, consumptions, loadBounds(name, objective)...)
 end
